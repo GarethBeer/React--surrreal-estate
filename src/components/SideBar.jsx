6787d266930aa3d241c22e39c23306c8d37ea4ff
GarethBeer <gareth.beer1989@outlook.com>
2019-12-16 12:06:08 +0000

    a few more style ttweaks on the form and nav

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import '../style/SideBar.css';
 
-const SideBar = () => (
-  <div className="sideBarContainer">
-    <h2>Filter</h2>
-    <div className="link-container">
-      <h3>City</h3>
-      <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
-      <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
-      <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
-      <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
+const SideBar = () => {
+  return (
+    <div className="sideBarContainer">
+      <h2>Filter</h2>
+      <div className="link-container">
+        <h3>City</h3>
+        <Link to={`/?query={"city": "Manchester"}`} className="link">
+          Manchester
+        </Link>
+        <Link to={`/?query={"city": "Liverpool"}`} className="link">
+          Liverpool
+        </Link>
+        <Link to={`/?query={"city": "Leeds"}`} className="link">
+          Leeds
+        </Link>
+        <Link to={`/?query={"city": "Sheffield"}`} className="link">
+          Sheffield
+        </Link>
+      </div>
+      <div className="link-container">
+        <h3>Price</h3>
+        <Link className="link">Link</Link>
+      </div>
     </div>
-    <div className="link-container">
-      <h3>Price</h3>
-      <Link to={`/?query={"price": < 60000}`}>0-60,000</Link>
-      <Link to={`/?query={"price": >60000 && < 80000}`}>60,000-80,000</Link>
-      <Link to={`/?query={"price": >80000 && < 100000}`}>80,000-100,000</Link>
-      <Link to={`/?query={"price": >100000}`}>100,000+</Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default SideBar;
